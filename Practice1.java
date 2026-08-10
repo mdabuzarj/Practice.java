54a898f18c1b88af61dd2b5342a51725920b495d
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-08-10 10:17:36 +0530

    Initial commit

---
+++ Practice1.java
@@ -0,0 +1,10 @@
+    class calculator{
+        int add(int a ,int b){
+            return a + b;
+        }
+
+    public static void main(String[] args) {
+        calculator c = new calculator();
+        System.out.println(c.add(7,8));
+    }
+}
